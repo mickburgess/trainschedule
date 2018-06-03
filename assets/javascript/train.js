32bd1b5b1413cd404beb9f99cc213cac03d9a358
Mick Burgess <mburgess@reliaslearning.com>
2018-06-03 19:51:16 -0400

    add images to jumbotron and logic for arrival time

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     // code for storing and retrieving train information
     trainName = $("#trainNameInput").val().trim();
     destination = $("#destinationInput").val().trim();
-    trainTime = $("#trainTimeInput").val().trim();
+    trainTime = moment($("#trainTimeInput").val().trim(), "HH:mm", true)
     frequency = $("#frequencyInput").val().trim();
     
     // add values to database
@@ -41,19 +41,55 @@ $(document).ready(function() {
     console.log(childSnapshot.val().destination);
     console.log(childSnapshot.val().firstTrainTime);
     console.log(childSnapshot.val().frequency);
-  
+    
+    // store frequency in a variable
+    var tFrequency = childSnapshot.val().frequency;
+
+    // store first train time in a variable
+    var firstTime = childSnapshot.val().firstTrainTime;
+
+    // firstTime converted to hour:minute format
+    var firstTimeConverted = moment(firstTime, "HH:mm");
+
+    // current time
+    var currentTime = moment();
+    console.log(moment(currentTime).format("HH:mm"));
+
+    // difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log(diffTime);
+
+    // time apart
+    var tRemainder = diffTime % tFrequency;
+    console.log(tRemainder);
+
+    // minutes until train
+    var tMinutesTillTrain = tFrequency - tRemainder;
+
+    // next train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    var nextTrainConversion = moment(nextTrain).format("hh:mm A");
+
     // create variable that will hold a "<tr>"
     var newRow = $("<tr>");
 
     // create variables to hold our table data
     var trainData = $("<td>").append(childSnapshot.val().train);
+
     var destinationData = $("<td>").append(childSnapshot.val().destination);
-    var frequencyData = $("<td>").append(childSnapshot.val().frequency);
+
+    var frequencyData = $("<td>").append(tFrequency);
+
+    var arrivalData = $("<td>").append(nextTrainConversion);
+
+    var minAwayData = $("<td>").append(tMinutesTillTrain);
 
     // append table data to the row
     newRow.append(trainData);
     newRow.append(destinationData);
     newRow.append(frequencyData);
+    newRow.append(arrivalData);
+    newRow.append(minAwayData);
 
     // append table row to table
     $("#tableBody").append(newRow);
